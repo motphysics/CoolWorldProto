95917c5c0e03e28ab214b6ff43b7f26d3161b722
motphysics <158263920+motphysics@users.noreply.github.com>
2025-11-02 23:48:20 -0800

    added intial commit swift files
    added files to be viewed on iphone

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoolWorldProto
+//
+//  Created by Arielle Corbo on 9/10/25.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("newhome")
+            .resizable()
+            .scaledToFill()
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoolWorldProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolWorldProtoApp.swift
+//  CoolWorldProto
+//
+//  Created by Arielle Corbo on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CoolWorldProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
